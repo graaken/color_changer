94265c215a89c5519b5ad3134db4464c7c8ef52d
grace kendall <grr.kendall@gmail.com>
2022-12-26 14:24:03 -0500

    added initial code

---
+++ app.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+btn.addEventListener('click', function() {
+    const newColor = makeRandColor();
+    document.body.style.backgroundColor = newColor;
+
+    h1.innerText = newColor;
+    
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return newColor = `rgb(${r}, ${g}, ${b})`
+}
