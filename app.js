0357f7f181c5139541fe59aa33ae8c7f503a3d32
grace kendall <grr.kendall@gmail.com>
2022-12-26 20:49:40 -0500

    added added minor tailwindcss styling + misc func

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ btn.addEventListener('click', function() {
     document.body.style.backgroundColor = newColor;
 
     h1.innerText = newColor;
-    
+
+    let r = parseInt(newColor.slice(4, 7));
+    let g = parseInt(newColor.slice(9, 12));
+    let b = parseInt(newColor.slice(14, 17));
+
+    if (r < 10 || g < 10 || b < 10) {
+        h1.style.color = 'white';
+    } else {
+        h1.style.color = 'black';
+    }
+})
+
+btn.addEventListener('mouseenter', function() {
+    btn.classList.toggle('bg-amber-500');
+})
+
+btn.addEventListener('mouseleave', function() {
+    btn.classList.toggle('bg-amber-500');
 })
 
 const makeRandColor = () => {
