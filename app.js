d9f06f040193ca0ca36833e121e16dbd904740f7
grace kendall <grr.kendall@gmail.com>
2023-01-01 22:51:54 -0500

    simplified css and some documentation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('button');
 const h1 = document.querySelector('h1');
 
+// when the user clicks on the 'click me' button, change the background color
+
 btn.addEventListener('click', function() {
     const newColor = makeRandColor();
     document.body.style.backgroundColor = newColor;
@@ -11,6 +13,8 @@ btn.addEventListener('click', function() {
     let g = parseInt(newColor.slice(9, 12));
     let b = parseInt(newColor.slice(14, 17));
 
+    // if any of the individual rgb color numbers are lower than 10 (typically a dark shade), make the text white. otherwise, make it black
+
     if (r < 10 || g < 10 || b < 10) {
         h1.style.color = 'white';
     } else {
@@ -18,13 +22,7 @@ btn.addEventListener('click', function() {
     }
 })
 
-btn.addEventListener('mouseenter', function() {
-    btn.classList.toggle('bg-amber-500');
-})
-
-btn.addEventListener('mouseleave', function() {
-    btn.classList.toggle('bg-amber-500');
-})
+// function for creating a random rgb code
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
@@ -32,3 +30,13 @@ const makeRandColor = () => {
     const b = Math.floor(Math.random() * 255);
     return newColor = `rgb(${r}, ${g}, ${b})`
 }
+
+// when the user clicks on the 'welcome' h1, replace it with a new input element
+
+h1.addEventListener('click', function() {
+    h1.style.display = 'none';
+    const rgbInput = document.createElement('p');
+    rgbInput.innerHTML = '<input type="text" id="rgbInput">';
+
+    h1.parentNode.replaceChild(rgbInput, h1);
+})
